50a9cec2910cf467e0a55d813377866d69465197
bianyuexu <81609292+bianyuexu@users.noreply.github.com>
2021-04-16 10:07:25 +0800

    Add files via upload

---
+++ ppp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+int main()
+{
+	int m = 24;
+	int n = 18;
+	int r = 0;
+	scanf("%d%d", &m, &n);
+	while (r=m%n)
+	{
+		m = n;
+		n = r;
+
+	}
+	printf("%d\n", n);
+	return 0;
+}
