cc0f169ef26d86e6eb5b74c286b8ab686324fed7
bianyuexu <81609292+bianyuexu@users.noreply.github.com>
2021-04-22 09:46:55 +0800

    Add files via upload

---
+++ add.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+//void Add(int* p)
+//{
+//	(*p)++;
+//}
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("num=%d\n", num); 
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	return 0;
+//}
+
+int main()
+//{
+//	
+//	printf("%d\n", strlen("abc"));
+//	return 0;
+//
+//}
+{
+	printf("%d", printf("%d", printf("%d", 43)));
+	return 0;
+}
