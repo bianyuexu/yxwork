f28e4adbae1625c86ee01aa03d5cd76153d8af30
bianyuexu <81609292+bianyuexu@users.noreply.github.com>
2021-05-12 10:39:47 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,7 @@
+#pragma once
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
--- test.c
+++ test.c
@@ -1,12 +1,47 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+#include<stdio.h>
+
+void menu()
+{
+	printf("******************************\n");
+	printf("********1.play 0.exit*********\n");
+	printf("******************************\n");
+}
+//游戏的整个算法实现
+void game()
+{
+	//数组存放走出的期盼信息
+	char board[ROW][COL] = { 0 };
+	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+}
+void test()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择\n");
+			break;
+		}
+	} while (input);
+}
+
 int main()
 {
-	int i = 0;
-	int arr[10] = { 1,2,3,4,5,6,7 };
-	while (i < 7)
-	{
-		printf("%d ", arr[i]);
-		i++;
-	}
+	test();
 	return 0;
 }
